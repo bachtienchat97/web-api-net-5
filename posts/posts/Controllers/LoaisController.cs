a4c044457f5d28705ac338f00daf2266c88d64e4
Bach Tien Chat <bachtienchat@gmail.com>
2023-04-21 09:53:31 +0700

    add source init

---
+++ posts/posts/Controllers/LoaisController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using posts.Data;
+using posts.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace posts.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoaisController : ControllerBase
+    {
+        private readonly MyDbContext _context;
+
+        public LoaisController(MyDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var dsLoai = _context.Loais.ToList();
+            return Ok(dsLoai);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var loai = _context.Loais.SingleOrDefault(item => item.MaLoai == id);
+            if (loai != null)
+            {
+                return Ok(loai);
+            }
+            return NotFound();
+        }
+
+        [HttpPost]
+        public IActionResult CreateNew(LoaiModel model)
+        {
+            try
+            {
+                var loai = new Loai
+                {
+                    TenLoai = model.TenLoai
+                };
+                _context.Add(loai);
+                _context.SaveChanges();
+                return Ok(loai);
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateById(int id, LoaiModel model)
+        {
+            var loai = _context.Loais.SingleOrDefault(loai => loai.MaLoai == id);
+                
+            if (loai != null)
+            {
+                loai.TenLoai = model.TenLoai;
+                _context.SaveChanges();
+                return NoContent();
+            }else
+            {
+                return NotFound();
+            }
+        }
+    }
+}
